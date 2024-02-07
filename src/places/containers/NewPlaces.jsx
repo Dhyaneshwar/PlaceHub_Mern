611d5dfcd86434474519d556a2d28db59d9e6530
Dhyaneshwar N <dhyaneshwar98@gmail.com>
2024-02-07 18:37:57 +0000

    feat: adding input validators

--- src/places/containers/NewPlaces.jsx
+++ src/places/containers/NewPlaces.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Input from "../../shared/components/FormElements/Input";
 
+import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
 import "./NewPlace.css";
 
 function NewPlaces() {
   return (
     <form className="place-form">
-      <Input element="input" type="text" label="Title" />
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid Title"
+      />
     </form>
   );
 }
